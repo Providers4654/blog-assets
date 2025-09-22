1a734f249f9ec29ec61578b11d6a48c5ad5fa0d9
Providers4654 <providers@mtnhlth.com>
2025-09-22 11:53:33 -0600

    Update blog.js

--- blog.js
+++ blog.js
@@ -155,10 +155,17 @@ if (relatedPosts.length) {
 
 
 
-
+  
   // Disclaimer (from CSS)
   const disclaimer = document.getElementById("globalDisclaimer");
   disclaimer.textContent = getComputedStyle(document.documentElement).getPropertyValue('--disclaimer-text').trim();
+
+  // === Hide loader and show blog ===
+  document.getElementById("loading-screen").style.display = "none";
+  document.querySelector(".blog-container").style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", loadSingleBlogPost);
+window.addEventListener("load", loadSingleBlogPost);
+
+
+
